2b7eefdb0eca1d7987f52bf623a486ff42d8c202
mohamedemad201900715 <medoem82950@gmail.com>
2023-12-03 11:44:03 +0200

    Upload our-team

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ const sr = ScrollReveal({
     // reset: true
 })
 sr.reveal(`.text`, {delay:400, origin:'left'})
-sr.reveal(`header , .main-heading`, {delay:200, origin:'top'})
-sr.reveal(`.supscribe form `, {delay:200, origin:'left'})
-sr.reveal(`.service-content .col, .supscribe p`, {delay:200, origin:'left'})
-sr.reveal(`.contact-head .content`, {delay:200, origin:'bottom'})
+sr.reveal(`header , .main-heading , .our-team .text`, {delay:200, origin:'top'})
+sr.reveal(`.supscribe form , .card ,.service-content .col, .supscribe p ,.info`, {delay:300, origin:'left'})
+sr.reveal(`.contact-head .content form`, {delay:200, origin:'bottom'})
 
